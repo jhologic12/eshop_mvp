69244587321b92f6d96e290657a87d5ab8c9d18c
Jhon Ospino <jaofdev@gmail.com>
2025-10-24 17:56:09 -0500

    se cofigura la rutaFix static files path and update CORS for local frontend

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,9 +1,14 @@
 # app/main.py
 from fastapi import FastAPI
 from fastapi.staticfiles import StaticFiles
-from app.api.routes import router as api_router
 from fastapi.middleware.cors import CORSMiddleware
+from app.api.routes import router as api_router
 from app.core.database import init_db
+from pathlib import Path
+import os
+FRONTEND_URL = os.getenv("FRONTEND_URL", "http://localhost:5173")
+
+BASE_DIR = Path(__file__).resolve().parent.parent  # backend/
 
 app = FastAPI(title="E-Shop MVP Backend")
 
@@ -11,12 +16,16 @@ app = FastAPI(title="E-Shop MVP Backend")
 init_db()
 
 # 🔹 Montar carpeta de archivos estáticos
-app.mount("/static", StaticFiles(directory="static"), name="static")
+STATIC_DIR = BASE_DIR / "static"
+if STATIC_DIR.exists():
+    app.mount("/static", StaticFiles(directory=STATIC_DIR), name="static")
+else:
+    raise RuntimeError(f"Directory '{STATIC_DIR}' does not exist")
 
-# 🔹 Configuración CORS (para desarrollo)
+# 🔹 Configuración CORS
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:5173"],  # o lista de URLs de tu frontend: ["http://localhost:5173"]
+    allow_origins=[FRONTEND_URL],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
